1c01cca45117ab65ff8c7f4455ef6c49c096b27c
sohan001 <sohan.singh@decurtis.com>
2015-12-23 17:40:25 +0530

    Added new console library project

---
+++ SamplePublishONGitHub/SampleLibPublishOnGitHub/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLibPublishOnGitHub
+{
+    public class Class1
+    {
+        public void test()
+        {
+            Console.WriteLine("Hello !!");
+
+        }
+    }
+}
